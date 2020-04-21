9ea00d67b9db68ddf0151050944ddbbdec86d149
OlRabiesAbe <45137811+OlRabiesAbe@users.noreply.github.com>
2020-04-20 18:30:24 -0700

    Add files via upload

--- tile.js
+++ tile.js
@@ -8,8 +8,7 @@ class Tile extends Entity {
 		
 		//collision specifics suite
 		this.HORZ_COLL_RADIUS = 16; //how far a tile's hitbox extends out to the left and right
-		this.VERT_LOWER_COLL_RADIUS = 32; //'' how far a tile's vertical hitboxes into the tile
-		this.VERT_UPPER_COLL_RADIUS = 0; // how far a tile's vertical hitboxes out of the tile
+		this.VERT_COLL_RADIUS = 32; //'' how far a tile's vertical hitboxes into the tile
 		this.ARB_HITBOX_BUFFER = 3; // pointless variable describing the buffer between different collision hitboxes, so they dont overlap
 									// i cant think of a better way to decribe it
 	}
@@ -17,33 +16,33 @@ class Tile extends Entity {
 	handleEntityCollision(entity) {
 		
 		//checking if the entity is colliding with this tile. last else = a collision is indeed occuring
-		if(entity.y < this.y - this.VERT_UPPER_COLL_RADIUS || entity.y - entity.height > this.y + this.height + this.VERT_UPPER_COLL_RADIUS) {}
+		/*if(entity.y < this.y - this.VERT_UPPER_COLL_RADIUS || entity.y - entity.height > this.y + this.height + this.VERT_UPPER_COLL_RADIUS) {}
 		else if (entity.x + (entity.width/2)+1 < this.x - this.HORZ_COLL_RADIUS || entity.x - (entity.width/2)-1 > this.x + this.width + this.HORZ_COLL_RADIUS) {}
 		
-		else { //a collision has occured, displace entity out of tile
+		else*/ { //a collision has occured, displace entity out of tile
 			//a tile's left/right colliders extends HCR units into and out of the tile
 					// ~+LEFT CASE+~
-			if(entity.x + (entity.width/2)+1 > this.x - this.HORZ_COLL_RADIUS && entity.x + (entity.width/2)+1 < this.x + this.HORZ_COLL_RADIUS 
+			if(entity.x + (entity.width/2) > this.x && entity.x + (entity.width/2) < this.x + this.HORZ_COLL_RADIUS 
 							&& entity.y > this.y + this.ARB_HITBOX_BUFFER && entity.y - entity.height < this.y + this.height - this.ARB_HITBOX_BUFFER) {
-				entity.x = this.x - this.HORZ_COLL_RADIUS - (entity.width/2)+1;
+				entity.x = this.x - (entity.width/2);
 				entity.hspeed = 0;
 			}
 					// ~+RIGHT CASE+~
-			if(entity.x - (entity.width/2)+1 < this.x + this.width + this.HORZ_COLL_RADIUS && entity.x - (entity.width/2)+1 > this.x + this.width - this.HORZ_COLL_RADIUS 
+			if(entity.x - (entity.width/2) < this.x + this.width && entity.x - (entity.width/2) > this.x + this.width - this.HORZ_COLL_RADIUS 
 							&& entity.y > this.y + this.ARB_HITBOX_BUFFER && entity.y - entity.height < this.y + this.height - this.ARB_HITBOX_BUFFER) {
-				entity.x = this.x + this.width + this.HORZ_COLL_RADIUS + (entity.width/2)+1;
+				entity.x = this.x + this.width + (entity.width/2);
 				entity.hspeed = 0;
 			}
 					// ~+TOP CASE+~
-			if(entity.y > this.y - this.VERT_UPPER_COLL_RADIUS && entity.y < this.y + this.VERT_LOWER_COLL_RADIUS 
+			if(entity.y > this.y && entity.y < this.y + this.VERT_COLL_RADIUS 
 							&& entity.x + (entity.width/2) > this.x + this.ARB_HITBOX_BUFFER && entity.x - (entity.width/2) < this.x + this.width - this.ARB_HITBOX_BUFFER) {
-				entity.y = this.y - this.VERT_UPPER_COLL_RADIUS;
+				entity.y = this.y;
 				entity.vspeed = 0;
 			}
 					// ~+BOTTOM CASE+~
-			if(entity.y - entity.height < this.y + this.height + this.VERT_UPPER_COLL_RADIUS && entity.y - entity.height > this.y + this.height - this.VERT_LOWER_COLL_RADIUS 
+			if(entity.y - entity.height < this.y + this.height && entity.y - entity.height > this.y + this.height - this.VERT_COLL_RADIUS 
 							&& entity.x + (entity.width/2) > this.x + this.ARB_HITBOX_BUFFER && entity.x - (entity.width/2) < this.x + this.width - this.ARB_HITBOX_BUFFER) {
-				entity.y = this.y + this.height + this.VERT_UPPER_COLL_RADIUS + entity.height;
+				entity.y = this.y + this.height + entity.height;
 				entity.vspeed = 0;
 			}
 		}
